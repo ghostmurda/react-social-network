fb8d92db2114e3ad96504bc3da998f5ba342200f
Daniel Simonov <sk8kmd@gmail.com>
2020-10-04 00:19:39 +0300

    Add position sticky to header, menu, secondary user list; optimize color vars

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { ReactComponent as Logo } from '../../img/logo.svg';
-import { ReactComponent as Search } from '../../img/search.svg';
+import {ReactComponent as Logo} from '../../img/logo.svg';
+import {ReactComponent as Search} from '../../img/search.svg';
 import './Header.css';
 
-function Header(props){
+function Header(props) {
     return (
         <div className="Header">
             <div className="header">
-            <div className="header__logo">
-                <Logo />
-            </div>
-            <div className="header__search">
-                <Search />
-                <input type="text" placeholder="Search" />
-            </div>
-            <div className="header__user">
-                <img src={`https://api.adorable.io/avatars/64/${props.userObj.name}@adorable.png`} alt="Avatar"/>
-            </div>
+                <div className="header__logo">
+                    <Logo/>
+                </div>
+                <div className="header__search">
+                    <Search/>
+                    <input type="text" placeholder="Search"/>
+                </div>
+                <div className="header__user">
+                    <img src={`https://api.adorable.io/avatars/64/${props.userObj.name}@adorable.png`} alt="Avatar"/>
+                </div>
             </div>
         </div>
     );
