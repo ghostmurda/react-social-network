2b98b8b680b47bf9ecde716a29270dcc25d0e4b6
Daniel Simonov <sk8kmd@gmail.com>
2020-10-04 01:00:21 +0300

    Change App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import state from "./Data/State";
 
 test('renders Home link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App appState={state}/>);
   const linkElement = getByText("Home");
   expect(linkElement).toBeInTheDocument();
 });
