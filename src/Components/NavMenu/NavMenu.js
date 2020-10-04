c516ccdf8ea644f39c2fe8383daa01b03ffa7fcf
Daniel Simonov <sk8kmd@gmail.com>
2020-10-04 11:34:26 +0300

    Change menu, add active classname

--- src/Components/NavMenu/NavMenu.js
+++ src/Components/NavMenu/NavMenu.js
@@ -3,15 +3,15 @@ import './NavMenu.css';
 import {ReactComponent as HomeIcon} from '../../img/home-icon.svg';
 import {ReactComponent as FriendsIcon} from '../../img/friends-icon.svg';
 import {ReactComponent as PopularIcon} from '../../img/popular-icon.svg';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 function NavMenu(){
     return (
         <div className="NavMenu">
             <ul>
-                <Link to="/react-social-network"><li><HomeIcon /> &nbsp; Home</li></Link>
-                <Link to="/following"><li><FriendsIcon /> &nbsp; Following</li></Link>
-                <Link to="/popular"><li><PopularIcon /> &nbsp; Popular</li></Link>
+                <NavLink to="/react-social-network" activeClassName="link-selected"><li><HomeIcon /> &nbsp; Home</li></NavLink>
+                <NavLink to="/following" activeClassName="link-selected"><li><FriendsIcon /> &nbsp; Following</li></NavLink>
+                <NavLink to="/popular" activeClassName="link-selected"><li><PopularIcon /> &nbsp; Popular</li></NavLink>
             </ul>
         </div>
     );
