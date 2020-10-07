56e6ef991187dba5d6101e60741622943b1c1b59
Daniel Simonov <sk8kmd@gmail.com>
2020-10-07 12:30:46 +0300

    Add chats prototype to dialogs page

---
+++ src/Components/Dialogs/ChatCard/ChatCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './ChatCard.css';
+import {Link} from "react-router-dom";
+
+function ChatCard(props){
+    return (
+        <div className="ChatCard">
+            <div className="chat-card">
+                <img
+                    className="chat-card__avatar"
+                    src={`https://api.adorable.io/avatars/128/${props.name}@adorable.png`}
+                    alt="Avatar"
+                />
+                <div className="chat-card__description">
+                    <div className="chat-card__name">
+                        <Link to="/dialogs/1">
+                            {props.name}
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ChatCard;
--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Dialogs.css';
+import ChatCard from "./ChatCard/ChatCard";
 
 function Dialogs(){
     return (
@@ -13,7 +14,9 @@ function Dialogs(){
                     <button className="btn">Send</button>
                 </div>
                 <div className="dialogs-page__chats">
-
+                    <div className="secondary-list__header">Chats</div>
+                    <span>&nbsp;</span>
+                    <ChatCard name="Daniel" />
                 </div>
             </div>
         </div>
