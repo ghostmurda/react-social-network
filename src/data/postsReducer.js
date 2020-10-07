bf8143c444399f2345be9730e4ed47cde2144d6a
Daniel Simonov <sk8kmd@gmail.com>
2020-10-07 23:30:33 +0300

    Refactor postReducer.js

--- src/data/postsReducer.js
+++ src/data/postsReducer.js
@@ -1,16 +1,20 @@
 const postsReducer = (state, currentUser, action) => {
-    if (action.type === 'SORT-POSTS'){
-        state[action.profile].sort((a, b) => b.id - a.id );
-    } else if (action.type === 'ADD-POST'){
-        let newPost = {
-            id: state[action.profile].length,
-            name: currentUser.userInfo.name,
-            text: action.postText,
-        };
-        state[action.profile].push(newPost);
-        state[action.profile].sort((a, b) => b.id - a.id );
+    switch (action.type){
+        case 'SORT-POSTS':
+            state[action.profile].sort((a, b) => b.id - a.id );
+            return state;
+        case 'ADD-POST':
+            let newPost = {
+                id: state[action.profile].length,
+                name: currentUser.userInfo.name,
+                text: action.postText,
+            };
+            state[action.profile].push(newPost);
+            state[action.profile].sort((a, b) => b.id - a.id );
+            return state;
+        default:
+            return state;
     }
-    return state;
 }
 
 export default postsReducer;
