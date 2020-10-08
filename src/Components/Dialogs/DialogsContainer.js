bc20b762491478028cf93f2933a7c0d93b9e490b
Daniel Simonov <sk8kmd@gmail.com>
2020-10-08 23:11:31 +0300

    Update Dialogs container component

--- src/Components/Dialogs/DialogsContainer.js
+++ src/Components/Dialogs/DialogsContainer.js
@@ -1,37 +1,7 @@
-import React from "react";
 import {withRouter} from 'react-router-dom';
 import {addMessageActionCreator} from "../../data/chatReducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
-
-// function DialogsContainer(props){
-//     let data;
-//     let textAreaDisabled = false;
-//     if (!props.location.state){
-//         data = 'No data';
-//         textAreaDisabled = true;
-//     } else {
-//         data = props.location.state;
-//     }
-//     let chatsElements = props.userListData.map((item, i) => {
-//         return <ChatCard
-//             key={i}
-//             name={item.name}
-//             chatData={props.chats[item.name]}
-//         />
-//     });
-//     let addMessage = (profile, text, messageCreator = props.userData.name) => {
-//         props.dispatch(addMessageActionCreator(profile, text, messageCreator));
-//     }
-//     return (
-//         <Dialogs
-//             addMessage={addMessage}
-//             chatsElements={chatsElements}
-//             textAreaDisabled={textAreaDisabled}
-//             data={data}
-//         />
-//     );
-// }
 
 let mapStateToProps = (state, ownProps) => {
     let messagesData;
