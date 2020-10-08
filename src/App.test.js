b8c0af6be1fb46427d67089afede9bd6499368a4
Daniel Simonov <sk8kmd@gmail.com>
2020-10-08 21:40:28 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 import store from "./data/store";
+import {Provider} from "react-redux";
 
 test('renders Home link', () => {
-  const { getByText } = render(<App appState={store.getState()}
-                                    dispatch={store.dispatch.bind(store)}/>
-                                    );
-  const linkElement = getByText("Home");
-  expect(linkElement).toBeInTheDocument();
+    const {getByText} = render(
+        <Provider store={store}>
+            <App appState={store.getState()}
+                 dispatch={store.dispatch.bind(store)}
+            />
+        </Provider>
+    );
+    const linkElement = getByText("Home");
+    expect(linkElement).toBeInTheDocument();
 });
