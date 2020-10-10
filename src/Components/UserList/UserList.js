73409673419a5a7ea370f6618aae813abcda6db8
Daniel Simonov <sk8kmd@gmail.com>
2020-10-10 23:48:32 +0300

    Refactor user list component

--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -3,49 +3,95 @@ import './UserList.css';
 import UserCard from './UserCard/UserCard';
 import * as axios from 'axios';
 
-function UserList(props) {
+class UserList extends React.Component{
+    constructor(props) {
+        super(props);
+        axios.get('http://test-social-network-api.herokuapp.com/users')
+            .then(responce => {
+                this.props.getUsers(responce.data);
+            })
+    }
 
-    axios.get('http://test-social-network-api.herokuapp.com/users')
-        .then(responce => {
-            props.getUsers(responce.data);
-        })
-
-    let userListElements = props.userListData.map((item, i) => {
-        return <UserCard
-            key={i}
-            name={item.name}
-            posts={item.posts}
-            followers={item.followers}
-            following={item.following}
-            info={item.info}
-        />;
-    });
-    let userSecondaryListElements = props.userSecondaryListData.map((item, i) => {
-        return <UserCard
-            key={i}
-            name={item.name}
-            posts={item.posts}
-            followers={item.followers}
-            following={item.following}
-            info={item.info}
-        />;
-    });
-    return (
-        <div className="UserList">
-            <div className="users-page">
-                <div className="users-page__list">
-                    {userListElements}
-                </div>
-                <div className="users-page__secondary-list">
-                    <div className="secondary-list__header">Popular</div>
-                    <span>&nbsp;</span>
-                    <div className="UserSmallCard">
-                        {userSecondaryListElements}
+    render() {
+        return (
+            <div className="UserList">
+                <div className="users-page">
+                    <div className="users-page__list">
+                        {this.props.userListData.map((item, i) => {
+                            return <UserCard
+                            key={i}
+                            name={item.name}
+                            posts={item.posts}
+                            followers={item.followers}
+                            following={item.following}
+                            info={item.info}
+                            />;
+                        })}
+                    </div>
+                    <div className="users-page__secondary-list">
+                        <div className="secondary-list__header">Popular</div>
+                        <span>&nbsp;</span>
+                        <div className="UserSmallCard">
+                            {this.props.userSecondaryListData.map((item, i) => {
+                                return <UserCard
+                                    key={i}
+                                    name={item.name}
+                                    posts={item.posts}
+                                    followers={item.followers}
+                                    following={item.following}
+                                    info={item.info}
+                                />;
+                            })}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
+
+// function UserList(props) {
+//     axios.get('http://test-social-network-api.herokuapp.com/users')
+//         .then(responce => {
+//             props.getUsers(responce.data);
+//         })
+//
+//     let userListElements = props.userListData.map((item, i) => {
+//         return <UserCard
+//             key={i}
+//             name={item.name}
+//             posts={item.posts}
+//             followers={item.followers}
+//             following={item.following}
+//             info={item.info}
+//         />;
+//     });
+//     let userSecondaryListElements = props.userSecondaryListData.map((item, i) => {
+//         return <UserCard
+//             key={i}
+//             name={item.name}
+//             posts={item.posts}
+//             followers={item.followers}
+//             following={item.following}
+//             info={item.info}
+//         />;
+//     });
+//     return (
+//         <div className="UserList">
+//             <div className="users-page">
+//                 <div className="users-page__list">
+//                     {userListElements}
+//                 </div>
+//                 <div className="users-page__secondary-list">
+//                     <div className="secondary-list__header">Popular</div>
+//                     <span>&nbsp;</span>
+//                     <div className="UserSmallCard">
+//                         {userSecondaryListElements}
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default UserList;
--- src/data/currentUserReducer.js
+++ src/data/currentUserReducer.js
@@ -2,7 +2,7 @@ const GET_USERS = 'GET_USERS';
 
 let initialState = {
     followingList: [
-        {name: "Pavel Durov", posts: "3", followers: "4", following: "20", info: "Creator of VK and Telegram"},
+        /*{name: "Pavel Durov", posts: "3", followers: "4", following: "20", info: "Creator of VK and Telegram"},
         {name: "Jon Snow", posts: "234", followers: "53", following: "5", info: "Son of Lyanna Stark and Rhaegar Targaryen"},
         {name: "Test User", posts: "0", followers: "0", following: "100", info: "Test info"},
         {name: "Dana West", posts: "427", followers: "568", following: "23", info: "I love myself"},
@@ -11,7 +11,7 @@ let initialState = {
         {name: "Taiba Moran", posts: "234", followers: "53", following: "5", info: "Test"},
         {name: "Kason Parry", posts: "0", followers: "0", following: "100", info: "Test"},
         {name: "Manha Hayden", posts: "427", followers: "568", following: "23", info: "Test"},
-        {name: "Cydney Power", posts: "15", followers: "99", following: "6", info: "Test"},
+        {name: "Cydney Power", posts: "15", followers: "99", following: "6", info: "Test"},*/
     ],
     userInfo: {
         name: 'Daniel Simonov',
