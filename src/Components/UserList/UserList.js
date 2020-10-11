f0f7c488971eecb10fa8aa8c7b5a23d169052589
Daniel Simonov <sk8kmd@gmail.com>
2020-10-11 12:18:11 +0300

    Refactor UserList class component, add ComponentDidMount

--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -4,9 +4,8 @@ import UserCard from './UserCard/UserCard';
 import * as axios from 'axios';
 
 class UserList extends React.Component{
-    constructor(props) {
-        super(props);
-        axios.get('https://test-social-network-api.herokuapp.com/users')
+    componentDidMount() {
+        axios.get('https://test-social-network-api.herokuapp.com/api/users')
             .then(responce => {
                 this.props.getUsers(responce.data);
             })
@@ -49,49 +48,5 @@ class UserList extends React.Component{
         );
     }
 }
-
-// function UserList(props) {
-//     axios.get('http://test-social-network-api.herokuapp.com/users')
-//         .then(responce => {
-//             props.getUsers(responce.data);
-//         })
-//
-//     let userListElements = props.userListData.map((item, i) => {
-//         return <UserCard
-//             key={i}
-//             name={item.name}
-//             posts={item.posts}
-//             followers={item.followers}
-//             following={item.following}
-//             info={item.info}
-//         />;
-//     });
-//     let userSecondaryListElements = props.userSecondaryListData.map((item, i) => {
-//         return <UserCard
-//             key={i}
-//             name={item.name}
-//             posts={item.posts}
-//             followers={item.followers}
-//             following={item.following}
-//             info={item.info}
-//         />;
-//     });
-//     return (
-//         <div className="UserList">
-//             <div className="users-page">
-//                 <div className="users-page__list">
-//                     {userListElements}
-//                 </div>
-//                 <div className="users-page__secondary-list">
-//                     <div className="secondary-list__header">Popular</div>
-//                     <span>&nbsp;</span>
-//                     <div className="UserSmallCard">
-//                         {userSecondaryListElements}
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
 
 export default UserList;
