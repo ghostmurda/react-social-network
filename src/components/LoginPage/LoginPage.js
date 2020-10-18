d667bcbdde065be470b3f701539ede25d79595a4
Daniel Simonov <sk8kmd@gmail.com>
2020-10-18 12:46:17 +0300

    Update login page form, add react-final-form, update markup

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,45 +1,56 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect} from "react";
+import {Form, Field} from 'react-final-form';
 import {useHistory} from 'react-router-dom';
 import './LoginPage.css';
 import Loader from "../Loader/Loader";
 
 function LoginPage(props) {
-    const [loginProcess, setLoginProcess] = useState(false);
     let history = useHistory();
-    const loginRef = useRef();
-    const passwordRef = useRef();
-    let setLoginProcessFalse = () => setLoginProcess(false);
-
-    let authFunc = () => {
-        if (loginRef.current.value !== '' && passwordRef.current.value !== '') {
-            setLoginProcess(true);
-        }
-    };
 
     useEffect(() => {
-        if (loginProcess) {
-            setLoginProcessFalse();
-            props.onLoginProcess(loginRef.current.value, passwordRef.current.value);
-            loginRef.current.value = '';
-            passwordRef.current.value = '';
-        }
-        if (props.authData.userId !== null){
+        if (props.authData.userId !== null) {
             history.push(`/home/${props.authData.userId}`);
         }
     });
+
+    const onSubmit = (values) => {
+        props.onLoginProcess(values.username, values.password);
+        values.username = '';
+        values.password = '';
+    }
 
     return (
         <>
             {props.authData.isFetching ? <Loader/> :
                 <div className="LoginPage">
-                    <div className="login-page__form">
-                        <input type="login" placeholder="Login" ref={loginRef}/>
-                        <input type="password" placeholder="Password" ref={passwordRef}/>
-                        <div className="from__btn-wrapper">
-                            <button className="btn" onClick={authFunc}>Sign in</button>
-                            <span>Forgot password?</span>
-                        </div>
-                    </div>
+                    <Form onSubmit={onSubmit}
+                          render={({handleSubmit, form, submitting, pristine, values}) => (
+                              <form className="login-page__form" onSubmit={handleSubmit}>
+                                  <Field name="username">
+                                      {({input, meta}) => (
+                                          <div>
+                                              <input className="form__input" {...input} type="login"
+                                                     placeholder="Login"/>
+                                          </div>
+                                      )}
+                                  </Field>
+                                  <Field name="password">
+                                      {({input, meta}) => (
+                                          <div>
+                                              <input className="form__input" {...input} type="password"
+                                                     placeholder="Password"/>
+                                          </div>
+                                      )}
+                                  </Field>
+                                  <div className="form__btn-wrapper">
+                                      <button className="btn" type="submit" disabled={submitting}>
+                                          Sign in
+                                      </button>
+                                      <span>Forgot password?</span>
+                                  </div>
+                              </form>
+                          )}
+                    />
                 </div>
             }
         </>
