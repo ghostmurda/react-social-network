135d0380eeda227255aee2e73f12592a24d98c8d
Daniel Simonov <sk8kmd@gmail.com>
2020-10-23 23:52:02 +0300

    Add login page description component

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -3,6 +3,7 @@ import {Form, Field} from 'react-final-form';
 import {useHistory} from 'react-router-dom';
 import './LoginPage.css';
 import Loader from "../Loader/Loader";
+import LoginPageDescription from "./LoginPageDescription/LoginPageDescription";
 
 function LoginPage(props) {
     let history = useHistory();
@@ -23,29 +24,7 @@ function LoginPage(props) {
         <>
             {props.isFetching ? <Loader additional={"login-page"}/> :
                 <div className="LoginPage">
-                    <div className="login-page__description">
-                        <span className="login-page__description__title">
-                            react-social-network
-                        </span>
-                        <span className="login-page__description__body">
-                            <ul>
-                                <li>This is simple <a href="https://github.com/ghostmurda/react-social-network">
-                                    prototype of social network</a> single-page application made using React + Redux
-                                </li>
-                                <li>
-                                    react-social-network is using his own <a href="https://github.com/ghostmurda/server-rest-api">
-                                    RESTful API service</a> made using Express + Lowdb
-                                </li>
-                                <li>
-                                    Right now there is no registration feature
-                                </li>
-                                <li>
-                                    To sign in use login <strong>admin</strong> and password <strong>admin</strong><br/>
-                                    or <i>any number from 2 to 11</i> for login and <strong>admin</strong> for password
-                                </li>
-                            </ul>
-                        </span>
-                    </div>
+                    <LoginPageDescription/>
                     <Form onSubmit={onSubmit}
                           validate={values => {
                               const errors = {}
---
+++ src/components/LoginPage/LoginPageDescription/LoginPageDescription.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function LoginPageDescription(){
+    return (
+        <div className="login-page__description">
+                        <span className="login-page__description__title">
+                            react-social-network
+                        </span>
+            <span className="login-page__description__body">
+                            <ul>
+                                <li>
+                                    Use login <strong>admin</strong> or <i>any number from 2 to 11</i> and password <strong>admin</strong> to sign in
+                                </li>
+                                <li>This is simple <a href="https://github.com/ghostmurda/react-social-network">
+                                    prototype of social network</a> single-page application made using React + Redux
+                                </li>
+                                <li>
+                                    react-social-network is using his own <a href="https://github.com/ghostmurda/server-rest-api">
+                                    RESTful API service</a> made using Express + Lowdb
+                                </li>
+                                <li>
+                                    Right now here is no registration feature
+                                </li>
+                            </ul>
+                        </span>
+        </div>
+    );
+}
+
+export default LoginPageDescription;
