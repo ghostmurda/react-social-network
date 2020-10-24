b03ab5428c44addcf82fd2b338c45a47f4264c31
Daniel Simonov <sk8kmd@gmail.com>
2020-10-24 10:55:10 +0300

    Update posts wall component

--- src/components/ProfilePage/PostsWall/PostsWall.js
+++ src/components/ProfilePage/PostsWall/PostsWall.js
@@ -4,15 +4,8 @@ import Post from "./Post/Post";
 function PostsWall(props){
     let postElements = [];
     if (props.postsData !== undefined){
-        postElements = props.postsData.map((item, i) => {
-            return <Post
-                key={i}
-                name={item.name}
-                text={item.text}
-            />;
-        });
+        postElements = props.postsData.map((item, i) => <Post key={i} {...item}/>);
     }
-
     return (
         <div className="PostsWall">
             {postElements}
